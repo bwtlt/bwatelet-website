eac254ea065257016de61c34b7b03374fa48cf53
bwtlt <bertrandwatelet@gmail.com>
2022-07-04 21:30:03 +0200

    remove About page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import {
 } from 'react-router-dom';
 import Home from './components/Home';
 import Projects from './components/Projects';
-import About from './components/About';
 
 const App = function () {
   return (
@@ -21,12 +20,7 @@ const App = function () {
               <NavLink to="/projects" className="nav-link">
                 Projects
               </NavLink>
-              <NavLink to="/blog" className="nav-link">
-                Blog
-              </NavLink>
-              <NavLink to="/about" className="nav-link">
-                About
-              </NavLink>
+              <a href="https://bwatelet.fr/blog" className="nav-link">Blog</a>
             </nav>
           </header>
 
@@ -35,7 +29,6 @@ const App = function () {
               <Routes>
                 <Route exact path="/" element={<Home />} />
                 <Route path="/projects" element={<Projects />} />
-                <Route path="/about" element={<About />} />
               </Routes>
             </div>
           </main>
