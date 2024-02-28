549746604266b4dfa4caf31156ebb8188f6ae721
bwtlt <bertrandwatelet@gmail.com>
2024-02-28 16:50:46 +0100

    add phone number

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import { React, useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
 import Button from 'react-bootstrap/Button';
 
 const INTERVAL_PERIOD = 120;
@@ -16,6 +16,7 @@ const Home = function () {
   const [textId, setId] = useState(0);
   const [charId, setCharId] = useState(0);
   const [text, setText] = useState(TEXTS[0]);
+  const [showPhoneNumber, setShowPhoneNumber] = useState(false);
 
   useEffect(() => {
     const intervalID = setInterval(() => {
@@ -53,7 +54,17 @@ const Home = function () {
       <div className="contact">
         <Button className="contact-me" href="mailto:contact@bwatelet.fr">
           <FontAwesomeIcon icon={faEnvelope} />
-          <span>Contactez-moi</span>
+          <span>Ecrivez-moi</span>
+        </Button>
+        <Button className="contact-me" onClick={() => setShowPhoneNumber(true)}>
+          <FontAwesomeIcon icon={faPhone} />
+          {showPhoneNumber
+            ? (
+              <a className="phone-number" href="tel:+33744725060">
+                +33 7 44 72 50 60
+              </a>
+            )
+            : <span>Appelez-moi</span>}
         </Button>
       </div>
     </div>
